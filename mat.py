64d070bdbcf6053d542ef4b2cade9b2e6a5292a4
Ashwin Innuganti <ashwininnuganti@ashwins-mbp.attlocal.net>
2018-06-22 11:19:01 -0400

    Edited mat.py

--- mat.py
+++ mat.py
@@ -17,12 +17,14 @@
 """
 __version__ = 0.1
 
-import discord
-import logging
 from discord.ext import commands
+import discord
+
+import logging
+import inspect
+
 import config
 
-# Set up logger
 logger = logging.getLogger("mat")
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler("mat.log", "w", "utf-8")
@@ -33,16 +35,40 @@ logger.addHandler(handler)
 class MAT(commands.AutoShardedBot):
 
     def __init__(self):
-        super().__init__(command_prefix=["!mat ", "mat/", "mat."],
+        super().__init__(command_prefix=["!mat ", "mat/", "mat.", "<@459559711210078209> "],
                          description="MAT's Bot",
+                         pm_help=None,
                          activity=discord.Game("!mat help"),
-                         max_messages=2500)
+                         fetch_offline_members=False)
+        self.remove_command("help")
+
+        members = inspect.getmembers(self)
+        for name, member in members:
+            if isinstance(member, commands.Command):
+                if member.parent is None:
+                    self.add_command(member)
 
     async def on_ready(self):
         print('Logged in as')
         print(bot.user.name)
         print(bot.user.id)
         print('------')
+
+    @commands.command()
+    async def help(self, ctx):
+        await ctx.send("Work in progress. My only command right now is `info`. My prefixes are `!mat `, `mat.`, `mat/`, or you could mention me.")
+
+    @commands.command()
+    async def info(self, ctx):
+        embed = discord.Embed(title="MAT's Bot", description="A open-source, general purpose Discord bot written in Python.", color=discord.Color.from_rgb(0, 60, 255))
+        embed.add_field(name="Version", value=str(__version__))
+        embed.add_field(name="Author", value="NinjaSnail1080#8581")
+        embed.add_field(name="Server Count", value=f"{len(bot.guilds)}")
+        embed.add_field(name="Library", value="discord.py")
+        embed.add_field(name="License", value="GPL v3.0")
+        embed.add_field(name="Github Repo", value="https://github.com/NinjaSnail1080/MATs-Bot")
+
+        await ctx.send(embed=embed)
 
     def run(self):
         super().run(config.TOKEN)
