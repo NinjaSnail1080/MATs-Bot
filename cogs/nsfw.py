0e3e48949ccc62182ff9ab6c013346ca477f51aa
Ashwin Innuganti <ashwininnuganti@ashwins-mbp.attlocal.net>
2018-07-04 01:54:23 -0400

    Added (not working yet) gonewild command

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -21,6 +21,8 @@ from discord.ext import commands
 import discord
 import aiohttp
 
+import random
+
 
 class NSFW:
     """NSFW commands"""
@@ -28,9 +30,30 @@ class NSFW:
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
     async def gonewild(self, ctx):
         """Sends a random post from r/gonewild"""
-        pass
+        # Heavy WIP
+        if ctx.channel.is_nsfw():
+            with ctx.channel.typing():
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(
+                        "https://www.reddit.com/r/gonewild/hot.json?sort=hot", headers=
+                        {"User-Agent": "mats-bot-reddit : v1.0 (by u/NinjaSnail1080)"}) as w:
+                        page = w.json
+                        embed = discord.Embed(color=mat_color)
+                        embed.set_image(url=random.choice(
+                            ["https://i.pinimg.com/736x/05/15/cf/0515cf0c3e92d83deae8d0c4d880ebd6"
+                             "--honeypot-penny.jpg", "http://ancensored.com/files/images/vthumbs/"
+                             "p/55b26456f635ac3dbd7910fe21f2ec8b_full.jpg", "http://www.celebset."
+                             "net/pics/P/3309Penny%20Baker%20(Mens%20Club).jpg", "https://i.pinim"
+                             "g.com/736x/e0/26/3c/e0263c9fca451d3779dc6d409ddeb896--bathing-beaut"
+                             "ies-bubble-baths.jpg"]))
+                        await ctx.send("**Heavily WIP. Not working yet**. When finished, this "
+                                    "command will send a random post from r/gonewild.\n\nFor "
+                                    "now, enjoy this picture of Penny Baker", embed=embed)
+        else:
+            await ctx.send("You must be in an NSFW channel to use that command.")
 
 
 def setup(bot):
