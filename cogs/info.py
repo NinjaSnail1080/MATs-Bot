900293fa7a44966e67f5fdd65bd1519207bd25d8
Ashwin Innuganti <ashwininnuganti@ashwins-mbp.attlocal.net>
2018-07-06 03:25:35 -0400

    Added serverinfo command

--- cogs/info.py
+++ cogs/info.py
@@ -20,6 +20,8 @@ from mat import __version__, mat_color
 from discord.ext import commands
 import discord
 
+import datetime
+
 
 class Info:
     """Information"""
@@ -33,16 +35,16 @@ class Info:
     async def help(self, ctx):
         """Help"""
         await ctx.send("**Work in progress**. My commands right now are `info`, `lenny`, "
-                       "`coinflip`, `diceroll`, `xkcd`, `ascii`, `kick`, and `randomkick`. My "
-                       "prefixes are `!mat `, `mat.`, `mat/`, or you could mention me. I also "
-                       "have numerous trigger words/phrases, which serve to amuse/infuriate the "
-                       "people of this server.\n\nNote: I can't be on all the time. Since Ninja "
-                       "has no way of hosting me 24/7 as of now, I can only be on when he "
-                       "manually runs the script.")
+                       "`coinflip`, `diceroll`, `serverinfo`, `xkcd`, `ascii`, `kick`, and "
+                       "`randomkick`. My prefixes are `!mat `, `mat.`, `mat/`, or you could "
+                       "mention me. I also have numerous trigger words/phrases, which serve to "
+                       "amuse/infuriate the people of this server.\n\nNote: I can't be on all "
+                       "the time. Since Ninja has no way of hosting me 24/7 as of now, I can "
+                       "only be on when he manually runs the script.")
 
     @commands.command()
     async def info(self, ctx):
-        """Info about the bot"""
+        """Info about me"""
         embed = discord.Embed(title="MAT's Bot", description="A open-source, general purpose "
                               "Discord bot written in Python.", color=mat_color)
         embed.add_field(name="Version", value=__version__)
@@ -55,6 +57,101 @@ class Info:
         embed.set_footer(text="Dedicated to WooMAT1417#1142")
 
         await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.guild_only()
+    async def serverinfo(self, ctx, id=None):
+        """Info about the server."""
+        if id is None:
+            s = ctx.channel.guild
+        else:
+            s = self.bot.get_guild(int(id))
+
+        if s is None:
+            await ctx.send("This isn't a valid server id. Try again with a different one.")
+        elif s is not None and s.unavailable:
+            await ctx.send("This server is unavailable. I can't collect info on it. Try again "
+                           "with a different id.")
+        else:
+            embed = discord.Embed(
+                title=s.name, description="Server ID: " + str(s.id), color=mat_color)
+            embed.set_thumbnail(url=s.icon_url)
+            embed.add_field(name="Members", value=s.member_count)
+            embed.add_field(name="Roles", value=len(s.roles))
+            embed.add_field(name="Text Channels", value=len(s.text_channels))
+            embed.add_field(name="Voice Channels", value=len(s.voice_channels))
+            embed.add_field(name="Categories", value=len(s.categories))
+            embed.add_field(name="Custom Emojis", value=len(s.emojis))
+            embed.add_field(name="Region", value=str(s.region).upper())
+            embed.add_field(
+                name="Verification Level", value=str(s.verification_level).capitalize())
+            if s.afk_channel is not None:
+                embed.add_field(
+                    name="AFK Channel", value=s.afk_channel.mention + " after " + str(
+                        s.afk_timeout // 60) + " minutes")
+            else:
+                embed.add_field(name="AFK Channel", value="No AFK channel")
+            embed.add_field(
+                name="Server Creation Date", value=s.created_at.strftime("%b %-d, %Y"))
+            if s.features:
+                embed.add_field(name="Server Features", value=", ".join(s.features),
+                inline=False)
+            embed.add_field(
+                name="Server Owner", value=str(s.owner) + " (User ID: " + str(s.owner_id) + ")",
+                inline=False)
+
+            delta = datetime.datetime.utcnow() - s.created_at
+
+            y = int(delta.total_seconds()) // 31536000 # * Number of seconds in a non-leap year
+            mo = int(delta.total_seconds()) // 2592000 % 12 #* Number of seconds in a 30 days
+            d = int(delta.total_seconds()) // 86400 % 30 #* Number of seconds in a day
+            h = int(delta.total_seconds()) // 3600 % 24 #* Number of seconds in an hour
+            mi = int(delta.total_seconds()) // 60 % 60
+            se = int(delta.total_seconds()) % 60
+            #! Dont change delta.totalseconds() to delta.seconds.
+            #! For reasons I don't understand, it doesn't work.
+
+            if y == 1:
+                year_s = " year"
+            else:
+                year_s = " years"
+            if mo == 1:
+                month_s = " month"
+            else:
+                month_s = " months"
+            if d == 1:
+                day_s = " day"
+            else:
+                day_s = " days"
+            if h == 1:
+                hour_s = " hour"
+            else:
+                hour_s = " hours"
+            if mi == 1:
+                minute_s = " minute"
+            else:
+                minute_s = " minutes"
+            if se == 1:
+                second_s = " second"
+            else:
+                second_s = " seconds"
+
+            footer = []
+            if y != 0:
+                footer.append(str(y) + year_s + ", ")
+            if mo != 0:
+                footer.append(str(mo) + month_s + ", ")
+            if d != 0:
+                footer.append(str(d) + day_s + ", ")
+            if h != 0:
+                footer.append(str(h) + hour_s + ", ")
+            if mi != 0:
+                footer.append(str(mi) + minute_s + ", ")
+            footer.append("and " + str(se) + second_s + ".")
+
+            embed.set_footer(text=s.name + " has been around for roughly " + "".join(footer))
+
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
