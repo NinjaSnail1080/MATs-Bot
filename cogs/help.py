2a4c91792bb1df9dda98768956f384ac7ca80849
Ashwin Innuganti <ashwininnuganti@ashwins-mbp.attlocal.net>
2018-07-08 21:46:57 -0400

    Improved help command (still heavy WIP)

--- cogs/help.py
+++ cogs/help.py
@@ -20,9 +20,8 @@ from mat import mat_color
 from discord.ext import commands
 import discord
 
-import re
-
-#TODO: Fix all this cause none of it works properly
+# list_prefixes = "**Prefixes**: `" + "` | `".join()) + "`"
+list_prefixes = "**Prefixes**: `!mat` | `mat.` | `mat/`"
 
 
 class Help:
@@ -33,41 +32,45 @@ class Help:
 
         self.bot.remove_command("help")
 
-    @commands.group()
-    async def help(self, ctx):
+    @commands.command()
+    async def help(self, ctx, cat=None):
         """HELP!"""
-        # if cat is None:
-        embed = discord.Embed(
-            title="MAT's Bot | Help Command", description="**Prefixes**: `!mat `, `mat/`, "
-            "`mat.`, or you could mention me\n**Categories**:", color=mat_color)
+        if cat is None:
+            embed = discord.Embed(
+                title="MAT's Bot | Help Command", description=list_prefixes + "\n**Categories**:",
+                color=mat_color)
 
-        embed.add_field(
-            name="<:confetti:464831811558572035> Fun", value="5 commands\n`<prefix> help "
-            "fun` for more info")
-        embed.add_field(
-            name="<:paint:464836778000515072> Image Manipulation", value="0 commands\n"
-            "`<prefix> help image` for more info")
-        embed.add_field(
-            name="<:info:464831966382915584> Info", value="2 commands\n`<prefix> help info` "
-            "for more info")
-        embed.add_field(
-            name="<:Fist:464833337983500289> Moderation", value="2 commands\n`<prefix> help "
-            "mod` for more info")
-        embed.add_field(
-            name=":notes: Music", value="0 commands\n`<prefix> help music` for more info")
-        embed.add_field(
-            name=":wink: NSFW", value="1 command\n`<prefix> help nsfw` for more info")
+            embed.add_field(
+                name="<:confetti:464831811558572035> Fun", value="5 commands\n`<prefix> help "
+                "fun` for more info")
+            embed.add_field(
+                name="<:paint:464836778000515072> Image Manipulation", value="0 commands\n"
+                "`<prefix> help image` for more info")
+            embed.add_field(
+                name="<:info:464831966382915584> Info", value="2 commands\n`<prefix> help info` "
+                "for more info")
+            embed.add_field(
+                name="<:Fist:464833337983500289> Moderation", value="2 commands\n`<prefix> help "
+                "mod` for more info")
+            embed.add_field(
+                name=":notes: Music", value="0 commands\n`<prefix> help music` for more info")
+            embed.add_field(
+                name=":wink: NSFW", value="1 command\n`<prefix> help nsfw` for more info")
 
-        await ctx.send(
-            content="**Note**: I can't be on all the time. Since Ninja has no way of hosting "
-            "me 24/7 as of now, I can only be on when he manually runs the script.",
-            embed=embed)
+            await ctx.send(
+                content="**Note**: I can't be on all the time. Since Ninja has no way of hosting "
+                "me 24/7 as of now, I can only be on when he manually runs the script.",
+                embed=embed)
 
-    @help.command()
+        elif cat == "fun":
+            await self.fun(ctx)
+
+        elif cat == "info":
+            await self.info(ctx)
+
     async def fun(self, ctx):
         embed = discord.Embed(
-            title="Help | Fun Commands", description="**Prefixes**: `!mat `, `mat/`, `mat.`, or "
-            "you could mention me", color=mat_color)
+            title="Help | Fun Commands", description=list_prefixes, color=mat_color)
 
         embed.add_field(
             name="ascii", value="Converts an image into ascii art. Will work for most "
@@ -76,10 +79,22 @@ class Help:
             name="coinflip", value="Flips a coin, pretty self-explanatory", inline=False)
         embed.add_field(
             name="diceroll", value="Rolls a dice. By default a 6-sided one though the "
-            "number of sides can be specified", inline=False)
+            "number of sides can be specified. Format like this: `<prefix> diceroll (OPTIONAL)<# "
+            "of sides>`", inline=False)
         embed.add_field(
             name="lenny", value="A list of Lenny faces for all your copypasting needs", inline=False)
         embed.add_field(name="xkcd", value="Posts a random xkcd comic", inline=False)
+
+    async def info(self, ctx):
+        embed = discord.Embed(
+            title="Help | Info Commands", description=list_prefixes, color=mat_color)
+
+        embed.add_field(name="info", value="Info about me!", inline=False)
+        embed.add_field(name="serverinfo", value="Info about the server", inline=False)
+        embed.add_field(
+            name="userinfo", value="Info about a user. By default it'll show your user info, but "
+            "you can specify a different member of your server. Format like this: `<prefix> "
+            "userinfo (OPTIONAL)<@mention user or user's id>`", inline=False)
 
         await ctx.send(embed=embed)
 
