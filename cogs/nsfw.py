240aa100628305281ad663f99fd1a6d105ffc243
Ashwin Innuganti <ashwininnuganti@ashwins-mbp.attlocal.net>
2018-07-21 12:40:50 -0400

    Minor edits

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -32,72 +32,72 @@ class NSFW:
         self.bot = bot
 
     @commands.command()
+    @commands.is_nsfw()
     @commands.guild_only()
     async def girl(self, ctx):
         """Sends a pic of a (usually nude) girl"""
+        try:
+            with ctx.channel.typing():
+                async with aiohttp.ClientSession().get(
+                    "https://russiasexygirls.com/?random") as w:
+                    soup = BeautifulSoup(await w.text(), "lxml")
 
-        if ctx.channel.is_nsfw():
-            try:
-                with ctx.channel.typing():
-                    async with aiohttp.ClientSession().get(
-                        "https://russiasexygirls.com/?random") as w:
-                        soup = BeautifulSoup(await w.text(), "lxml")
+                    url = str(w.url)
+                    title = soup.find("span", "entry-title").get_text()
 
-                        url = str(w.url)
-                        title = soup.find("span", "entry-title").get_text()
+                    pics = []
+                    for p in soup.find_all("p"):
+                        if "https://russiasexygirls.com/" in str(p):
+                            pics.append(p)
+                    del pics[0]
+                    del pics[0]
+                    image = random.choice(pics).a.img["src"]
 
-                        pics = []
-                        for p in soup.find_all("p"):
-                            if "https://russiasexygirls.com/" in str(p):
-                                pics.append(p)
-                        del pics[0]
-                        del pics[0]
-                        image = random.choice(pics).a.img["src"]
+                embed = discord.Embed(
+                    title=title, description="Click above for more images from this album",
+                    color=find_color(ctx), url=url)
+                embed.set_image(url=image)
+                embed.set_footer(text="From: russiasexygirls.com")
 
-                    embed = discord.Embed(
-                        title=title, description="Click above for more images from this album",
-                        color=find_color(ctx), url=url)
-                    embed.set_image(url=image)
-                    embed.set_footer(text="From: russiasexygirls.com")
-
-                    await ctx.send(embed=embed)
-            except:
-                #! Temporary operation to figure out exactly what this exception is
-                import traceback
-                print(traceback.format_exc())
-                #! Temporary operation ^
-                await ctx.send("Huh, something went wrong. It looks like servers are down so I "
-                                "wasn't able to get a picture. Try again in a little bit.")
-        else:
-            await ctx.send("You must be in an NSFW channel to use that command.")
+                await ctx.send(embed=embed)
+        except:
+            #! Temporary operation to figure out exactly what this exception is
+            import traceback
+            print(traceback.format_exc())
+            #! Temporary operation ^
+            await ctx.send("Huh, something went wrong. It looks like servers are down so I "
+                            "wasn't able to get a picture. Try again in a little bit.")
 
     @commands.command()
+    @commands.is_nsfw()
     @commands.guild_only()
     async def gonewild(self, ctx):
         """Sends a random post from r/gonewild (Not actually working yet. Heavy WIP)"""
 
         #* Heavy WIP
-        if ctx.channel.is_nsfw():
-            with ctx.channel.typing():
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(
-                        "https://www.reddit.com/r/gonewild/hot.json?sort=hot", headers=
-                        {"User-Agent": "mats-bot-reddit : v1.0 (by u/NinjaSnail1080)"}) as w:
-                        page = await w.json
+        with ctx.channel.typing():
+            async with aiohttp.ClientSession() as session:
+                async with session.get(
+                    "https://www.reddit.com/r/gonewild/hot.json?sort=hot", headers=
+                    {"User-Agent": "mats-bot-reddit : v1.0 (by u/NinjaSnail1080)"}) as w:
+                    page = w.json
 
-                        embed = discord.Embed(color=find_color(ctx))
-                        embed.set_image(url=random.choice(
-                            ["https://i.pinimg.com/736x/05/15/cf/0515cf0c3e92d83deae8d0c4d880ebd6"
-                             "--honeypot-penny.jpg", "http://ancensored.com/files/images/vthumbs/"
-                             "p/55b26456f635ac3dbd7910fe21f2ec8b_full.jpg", "http://www.celebset."
-                             "net/pics/P/3309Penny%20Baker%20(Mens%20Club).jpg", "https://i.pinim"
-                             "g.com/736x/e0/26/3c/e0263c9fca451d3779dc6d409ddeb896--bathing-beaut"
-                             "ies-bubble-baths.jpg"]))
-                        await ctx.send("**Heavily WIP. Not working yet**. When finished, this "
-                                    "command will send a random post from r/gonewild.\n\nFor "
-                                    "now, please enjoy this picture of Penny Baker", embed=embed)
-        else:
-            await ctx.send("You must be in an NSFW channel to use that command.")
+                    embed = discord.Embed(color=find_color(ctx))
+                    embed.set_image(url=random.choice(
+                        ["https://i.pinimg.com/736x/05/15/cf/0515cf0c3e92d83deae8d0c4d880ebd6"
+                         "--honeypot-penny.jpg", "http://ancensored.com/files/images/vthumbs/"
+                         "p/55b26456f635ac3dbd7910fe21f2ec8b_full.jpg", "http://www.celebset."
+                         "net/pics/P/3309Penny%20Baker%20(Mens%20Club).jpg", "https://i.pinim"
+                         "g.com/736x/e0/26/3c/e0263c9fca451d3779dc6d409ddeb896--bathing-beaut"
+                         "ies-bubble-baths.jpg"]))
+                    await ctx.send("**Heavily WIP. Not working yet**. When finished, this "
+                                "command will send a random post from r/gonewild.\n\nFor "
+                                "now, please enjoy this picture of Penny Baker", embed=embed)
+
+    @commands.command()
+    @commands.is_nsfw()
+    async def test(self, ctx):
+        await ctx.send("test")
 
 
 def setup(bot):
