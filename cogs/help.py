5723bb0c16133616e104a954bbabfb2a978bf3ff
Ashwin Innuganti <ashwininnuganti@ashwins-mbp.attlocal.net>
2018-07-21 15:59:28 -0400

    Updated help command

--- cogs/help.py
+++ cogs/help.py
@@ -33,31 +33,30 @@ class Help:
         self.bot.remove_command("help")
 
     @commands.command()
-    @commands.guild_only()
     async def help(self, ctx, cat=None):
         """MAT's Bot | Help command"""
 
         if cat is None:
             embed = discord.Embed(
                 title="MAT's Bot | Help command", description=list_prefixes + "\n**Categories**:",
-                color=find_color(ctx.channel.guild))
+                color=find_color(ctx))
 
             embed.add_field(
-                name="<:confetti:464831811558572035> Fun", value="5 commands\n`<prefix> help "
-                "fun` for more info", inline=False)
+                name="<:confetti:464831811558572035> Fun", value="8 commands\n`<prefix> help "
+                "fun` for more info")
             embed.add_field(
-                name="<:info:464831966382915584> Info", value="2 commands\n`<prefix> help info` "
-                "for more info", inline=False)
+                name="<:info:464831966382915584> Info", value="5 commands\n`<prefix> help info` "
+                "for more info")
             embed.add_field(
                 name="<:Fist:464833337983500289> Moderation", value="2 commands\n`<prefix> help "
-                "mod` for more info", inline=False)
+                "mod` for more info")
             embed.add_field(
-                name=":notes: Music", value="0 commands\n`<prefix> help music` for more info",
-                inline=False)
+                name=":notes: Music", value="0 commands\n`<prefix> help music` for more info")
             embed.add_field(
-                name=":wink: NSFW", value="1 command\n`<prefix> help nsfw` for more info",
-                inline=False)
-            embed.set_footer(text="Do \"<prefix> help all\" for a list of all my commands")
+                name=":wink: NSFW", value="2 commands\n`<prefix> help nsfw` for more info")
+            embed.add_field(
+                name=":tools: Utility", value="2 commands\n`<prefix> help nsfw` for more info")
+            embed.set_footer(text="Do \"<prefix> help all\" for a list of all of my commands")
 
             await ctx.send(
                 content="**Note**: I can't be on all the time. Since Ninja has no way of hosting "
@@ -66,7 +65,7 @@ class Help:
 
         elif cat == "fun":
             embed = discord.Embed(title="Help | Fun Commands", description=list_prefixes,
-                                  color=find_color(ctx.channel.guild))
+                                  color=find_color(ctx))
 
             embed.set_author(name="MAT's Bot")
             for c in self.bot.commands:
@@ -80,7 +79,7 @@ class Help:
 
         elif cat == "info":
             embed = discord.Embed(title="Help | Information Commands", description=list_prefixes,
-                                  color=find_color(ctx.channel.guild))
+                                  color=find_color(ctx))
 
             embed.set_author(name="MAT's Bot")
             for c in self.bot.commands:
@@ -91,7 +90,7 @@ class Help:
 
         elif cat == "mod":
             embed = discord.Embed(title="Help | Moderation Commands", description=list_prefixes,
-                                  color=find_color(ctx.channel.guild))
+                                  color=find_color(ctx))
 
             embed.set_author(name="MAT's Bot")
             for c in self.bot.commands:
@@ -104,8 +103,8 @@ class Help:
             await ctx.send("No commands yet ¯\_(ツ)_/¯")
 
         elif cat == "utility":
-            embed = discord.Embed(title="Help | NSFW Commands", description=list_prefixes,
-                                  color=find_color(ctx.channel.guild))
+            embed = discord.Embed(title="Help | Utility Commands", description=list_prefixes,
+                                  color=find_color(ctx))
 
             embed.set_author(name="MAT's Bot")
             for c in self.bot.commands:
@@ -116,7 +115,7 @@ class Help:
 
         elif cat == "nsfw":
             embed = discord.Embed(title="Help | NSFW Commands", description=list_prefixes,
-                                  color=find_color(ctx.channel.guild))
+                                  color=find_color(ctx))
 
             embed.set_author(name="MAT's Bot")
             for c in self.bot.commands:
@@ -126,11 +125,57 @@ class Help:
             await ctx.send(embed=embed)
 
         elif cat == "all":
-            await ctx.send("Work in progress ¯\_(ツ)_/¯")
+            embed = discord.Embed(
+                title="Help | All Commands", description=list_prefixes, color=find_color(ctx))
+            embed.set_author(name="MAT's Bot")
+            embed.set_footer(
+                text="Do \"<prefix> help <command name>\" for help on a specific command")
+
+            embed.add_field(
+                name="<:confetti:464831811558572035> Fun",
+                value=", ".join([f"`{c.name}`" for c in self.bot.commands if c.cog_name == "Fun"
+                                 and not c.hidden]), inline=False)
+            embed.add_field(
+                name="<:info:464831966382915584> Info",
+                value=", ".join([f"`{c.name}`" for c in self.bot.commands if c.cog_name == "Info"
+                                 and not c.hidden]), inline=False)
+            embed.add_field(
+                name="<:Fist:464833337983500289> Moderation",
+                value=", ".join([f"`{c.name}`" for c in self.bot.commands
+                                 if c.cog_name == "Moderation" and not c.hidden]), inline=False)
+            embed.add_field(
+                name=":notes: Music", value="No commands yet ¯\_(ツ)_/¯", inline=False)
+            embed.add_field(
+                name=":wink: NSFW",
+                value=", ".join([f"`{c.name}`" for c in self.bot.commands if c.cog_name == "NSFW"
+                                 and not c.hidden]), inline=False)
+            embed.add_field(
+                name=":tools: Utility",
+                value=", ".join([f"`{c.name}`" for c in self.bot.commands
+                                 if c.cog_name == "Utility" and not c.hidden]), inline=False)
+
+            await ctx.send(embed=embed)
+
         else:
-            await ctx.send("That's not a category. The ones you can pick are:\n`fun` (Fun "
-                           "commands)\n`info` (Information commands)\n`mod` (Moderation commands)"
-                           "\n`music` (Music commands)\n`nsfw` (NSFW commands)")
+            sent = False
+            for cmd in self.bot.commands:
+                if cat == cmd.name:
+                    embed = discord.Embed(
+                        title="Help | %s Command" % cmd.name, description=cmd.help,
+                        color=find_color(ctx))
+                    embed.set_author(name="MAT's Bot")
+
+                    if cmd.aliases:
+                        embed.add_field(name="Aliases", value="`" + ", ".join(cmd.aliases) + "`")
+
+                    await ctx.send(embed=embed)
+                    sent = True
+                    break
+            if not sent:
+                await ctx.send("That's not a category. The ones you can pick are:\n\n`fun` (Fun "
+                            "commands)\n`info` (Information commands)\n`mod` (Moderation commands"
+                            ")\n`music` (Music commands)\n`nsfw` (NSFW commands)\n\nYou can also "
+                            "put the name of a command for help on that command only")
 
 
 def setup(bot):
