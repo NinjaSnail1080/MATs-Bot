499488345dfba11a6def3eea4fdb816acf01ef66
Ashwin Innuganti <ashwininnuganti@ashwins-mbp.attlocal.net>
2018-07-24 00:37:09 -0400

    Added armageddon command

--- cogs/owner.py
+++ cogs/owner.py
@@ -18,6 +18,11 @@
 
 from discord.ext import commands
 import discord
+import asyncio
+
+import time
+
+import mat
 
 
 class Owner:
@@ -38,6 +43,47 @@ class Owner:
         await self.bot.wait_until_ready()
         await self.bot.logout()
         await self.bot.close()
+
+    @commands.command()
+    @commands.is_owner()
+    async def armageddon(self, ctx, runtime: float=3.0):
+        """Unleash hell upon a Discord server"""
+
+        async def the_end(ctx, runtime: float):
+            """Background task for armageddon command"""
+
+            await ctx.send("This is the end")
+            await asyncio.sleep(2)
+            await ctx.send("The world is about to meet its demise")
+            await asyncio.sleep(2)
+            await ctx.send("Armageddon, Judgement Day, The Apocalypse")
+            await asyncio.sleep(1)
+            await ctx.send("It's upon us")
+            await asyncio.sleep(2)
+            await ctx.send("It will happen")
+            await asyncio.sleep(2)
+            await ctx.send("We are all going to die")
+            await asyncio.sleep(2)
+            await ctx.send("Goodbye, my friends")
+            await asyncio.sleep(3)
+
+            await ctx.send("T - 5")
+            await asyncio.sleep(1)
+            await ctx.send("4")
+            await asyncio.sleep(1)
+            await ctx.send("3")
+            await asyncio.sleep(1)
+            await ctx.send("2")
+            await asyncio.sleep(1)
+            await ctx.send("1")
+            await asyncio.sleep(1)
+
+            t = time.time()
+            while time.time() < t + runtime:
+                await ctx.send("@everyone")
+                await asyncio.sleep(0.5)
+
+        self.bot.loop.create_task(the_end(ctx, runtime * 60))
 
 
 def setup(bot):
