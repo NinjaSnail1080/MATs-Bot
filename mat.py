de58742bfb5cab3564ff0c1f427c58eaf04d3478
Ashwin Innuganti <ashwininnuganti@Ashwins-MacBook-Pro.local>
2018-09-30 23:05:30 -0400

    Version 0.6.2

--- mat.py
+++ mat.py
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-__version__ = 0.6
+__version__ = "0.6.2"
 
 from discord.ext import commands
 import discord
