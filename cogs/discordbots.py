622e814936084326a59f252b873dc4166bb61ebb
Ashwin Innuganti <ashwininnuganti@Ashwins-MacBook-Pro.local>
2019-07-27 01:13:48 -0400

    Added discordbots cog; it can't be used until I get the API token though

---
+++ cogs/discordbots.py
@@ -0,0 +1,43 @@
+"""
+    MAT's Bot: An open-source, general purpose Discord bot written in Python.
+    Copyright (C) 2018  NinjaSnail1080  (Discord User: @NinjaSnail1080#8581)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from discord.ext import commands
+import discord
+import dbl
+
+import config
+
+
+class DiscordBotsAPI(commands.Cog):
+    """Handles interactions with the discordbots.org API"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+        self.bot.dbl = dbl.Client(self.bot, config.DBL_TOKEN, session=self.bot.session)
+
+    def cog_unload(self):
+        self.bot.loop.run_until_complete(self.bot.dbl.close())
+
+    @commands.Cog.listener()
+    async def on_dbl_vote(self, data):
+        #TODO: Eventually this will contain economy stuff
+        pass
+
+
+def setup(bot):
+    bot.add_cog(DiscordBotsAPI(bot))
