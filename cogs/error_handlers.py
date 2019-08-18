aaec8bd9eb83e401262cc1a0dd2596764b6327cd
Ashwin Innuganti <ashwininnuganti@Ashwins-MacBook-Pro.local>
2019-08-18 01:50:17 -0400

    Edited CommandOnCooldown handler to add voting rewards and added VoteRequired handler + Fixed typoi

--- cogs/error_handlers.py
+++ cogs/error_handlers.py
@@ -16,7 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from utils import delete_message, find_color, CommandDisabled, ChannelNotNSFW
+from utils import delete_message, find_color, CommandDisabled, ChannelNotNSFW, VoteRequired
 
 from discord.ext import commands
 import discord
@@ -56,30 +56,61 @@ class Error_Handlers(commands.Cog):
             return await delete_message(ctx, 7)
 
 
+        elif isinstance(exc, VoteRequired):
+            embed = discord.Embed(
+                title="\U0001f44d Remember to vote!",
+                description="Voting is required to use this command. On top of that, voting will "
+                            "also allow you to have shorter command cooldowns. You can vote by "
+                            "clicking the link above",
+                url="https://discordbots.org/bot/459559711210078209/vote",
+                color=find_color(ctx))
+            await ctx.send(embed=embed, delete_after=45.0)
+            return await delete_message(ctx, 45)
+
+
         elif isinstance(exc, commands.CommandOnCooldown):
+            c = exc.cooldown
+            retry_after = exc.retry_after
+
             if await self.bot.is_owner(ctx.author):
                 return await ctx.reinvoke()
 
-            c = exc.cooldown
+            #* Voting reward
+            if await self.bot.dbl.get_user_vote(ctx.author.id):
+                if c.per - exc.retry_after >= c.per * (2/3):
+                    return ctx.reinvoke()
+                retry_after = (c.per * (2/3)) - (c.per - exc.retry_after)
+
             embed = discord.Embed(
-                description="__This command is on cooldown__. Try again in **"
-                            f"{round(exc.retry_after, 2)}** seconds.\n\n**Default Cooldown**: "
-                            f"{c.rate} use{'' if c.rate == 1 else 's'} every {int(c.per)} "
+                title="This command is on cooldown",
+                description=f"Try again in **{round(retry_after, 2)}** seconds.\n\n**Default "
+                            f"Cooldown**: {c.rate} use{'' if c.rate == 1 else 's'} every "
+                            f"{int(c.per)} second{'' if c.per == 1 else 's'}\n**[Voter](https://"
+                            f"discordbots.org/bot/459559711210078209/vote) Cooldown**: {c.rate} "
+                            f"use{'' if c.rate == 1 else 's'} every {int(c.per * (2/3))} "
                             f"second{'' if c.per == 1 else 's'}",
                 color=find_color(ctx))
-            await ctx.send(embed=embed)
-            await delete_message(ctx, 15)
+            #*Voting reward
+            if await self.bot.dbl.get_user_vote(ctx.author.id):
+                embed.set_footer(text=f"{ctx.author.display_name} has voted for MAT's Bot, so "
+                                      "they get shorter cooldowns",
+                                 icon_url=ctx.author.avatar_url)
+            else:
+                embed.set_footer(
+                    text="Vote for MAT's Bot using the link above to get shorter cooldowns")
+            await ctx.send(embed=embed, delete_after=30)
+            await delete_message(ctx, 30)
 
             #* Voting reward
-            # if await self.bot.dbl.get_user_vote(ctx.author.id):
-            #     await asyncio.sleep(c.per * (2/3))
-            #     await ctx.reinvoke()
+            if await self.bot.dbl.get_user_vote(ctx.author.id):
+                await asyncio.sleep(c.per * (2/3))
+                ctx.command.reset_cooldown(ctx)
             return
 
 
         elif isinstance(exc, commands.NotOwner):
             await ctx.send(
-                f"Only my owner, {self.bot.owner.name}, can use that command",
+                f"Only my owner, {self.bot.owner}, can use that command",
                 delete_after=6.0)
             return await delete_message(ctx, 6)
 
@@ -138,9 +169,9 @@ class Error_Handlers(commands.Cog):
             return await ctx.send(
                 f"```Command: {ctx.command.qualified_name}\n{exc}```An unknown error occured "
                 "and I wasn't able to complete that command. Sorry!\n\nPlease get in touch with "
-                f"my owner, {self.bot.owner.mention}, and tell him what happened so he can try "
+                f"my owner, **{self.bot.owner}**, and tell him what happened so he can try "
                 "and fix this issue. You can reach him at my support server: "
-                "https://discord.gg/P4Fp3jA")
+                "https://discord.gg/khGGxxj")
 
 
 def setup(bot):
